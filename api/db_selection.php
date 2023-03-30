deb93fdd559bf46f0983e067998df240dcfe68cd
makafui2 <makafui2@students.cs.ubc.ca>
2023-03-30 02:28:32 -0700

    *updated db_selection.php to include both projection and selection*

--- api/db_selection.php
+++ api/db_selection.php
@@ -1,16 +1,32 @@
 <?php
 require_once('db_connect.php');
 
-function handleSelectionRequest($table, $fields = null) {
+function handleSelectionRequest($table, $condition = null,$fields = null) {
     global $db_conn;
     $output = "<h2> Table: $table </h2>";
 
-    if (is_null($fields)) {
-        $result = executePlainSQL("SELECT * FROM $table");
-    }else {
-//        WILL USE THIS FOR PROJECTION
-        $selected_fields = implode(", ", $fields);
-        $result = executePlainSQL("SELECT $selected_fields FROM $table");
+    if (is_null($condition)) {
+        if (is_null($fields)) {
+
+            // Basic select : everything
+            $result = executePlainSQL("SELECT * FROM $table");
+        } else {
+
+            // projection with different fields
+            $selected_fields = implode(", ", $fields);
+            $result = executePlainSQL("SELECT $selected_fields FROM $table");
+        }
+    } else {
+        if (is_null($fields)) {
+
+            //Select with a condition (no projection)
+            $result = executePlainSQL("SELECT * FROM $table WHERE $condition");
+        } else {
+
+            // Select with condition  and projection
+            $selected_fields = implode(", ", $fields);
+            $result = executePlainSQL("SELECT $selected_fields FROM $table WHERE $condition");
+        }
     }
     
     $num_fields = oci_num_fields($result);
@@ -30,4 +46,3 @@ function handleSelectionRequest($table, $fields = null) {
     $output .=  "</table>";
     echo $output;
 }
-?>
