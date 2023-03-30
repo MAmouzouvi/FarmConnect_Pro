09f77e254f54fcc622331f97709873b54ca67700
makafui2 <makafui2@students.cs.ubc.ca>
2023-03-30 03:09:26 -0700

    *updated db_selection_by_date.php and db_selection_by_delivery_status.php*

--- api/db_selection_by_date.php
+++ api/db_selection_by_date.php
@@ -1,27 +1,26 @@
 <?php
+require_once('db_connect.php');
 function handleSelectionByDateRequest()
 {
     global $db_conn;
 
-    $date = date('YYYY-MM-DD');
+    $date = date("YYYY-MM-DD");
 
-    $query = "SELECT  CUSTOMERID, TOTALCOST, TRANSPORTSTATUS, DRIVERLICENSENUMBER, WAREHOUSENAME, DESTINATION, SCHEDULEDDATE
-FROM DELIVERY 
-WHERE DELIVERY.SCHEDULEDDATE= TO_DATE($date,'YYYY-MM-DD')";
 
-    $result = executePlainSQL($query);
 
-    if (($row = oci_fetch_row($result)) != false) {
-        echo "<table>";
-        echo "<tr><th>CustomerID</th><th>Total Cost</th><th>Transport Status</th><th>Driver's license number</th><th>Warehouse name</th><th>Destination</th><th>scheduled date</th></tr>";
+    $result = executePlainSQL("SELECT * FROM DELIVERY WHERE DELIVERY.SCHEDULEDDATE = $date");
 
-        while ($row = oci_fetch_array($result, OCI_BOTH)) {
-            echo "<tr><td>" . $row[0] . "</td><td>" . $row[1] . "</td><td>" . $row[2] . "</td><td>" . $row[3] . "</td><td>" . $row[4] . "</td><td>" . $row[5] . "</td><td>" . $row[6]. "</td></tr>";
+    if ((oci_fetch_row($result))) {
+        handleSelectionRequest("Delivery","Delivery.scheduledDate = $date");
 
-        }
-        echo "</table>";
-    } else {
+    }
+    else {
         echo "No deliveries found";
     }
 }
+
+
+
+
+
 
--- api/db_selection_by_delivery_status.php
+++ api/db_selection_by_delivery_status.php
@@ -1,26 +1,19 @@
 <?php
-function handleSelectionByDateRequest()
+require_once('db_connect.php');
+function handleSelectionByDeliveryStatusRequest()
 {
     global $db_conn;
 
-    $transport_status = "in-delivery"; //CHANGE to user imput value later
+    $status = " ";
 
-    $query = "SELECT  CUSTOMERID, TOTALCOST, TRANSPORTSTATUS, DRIVERLICENSENUMBER, WAREHOUSENAME, DESTINATION, SCHEDULEDDATE
-FROM DELIVERY 
-WHERE TRANSPORTSTATUS = '$transport_status'";
 
-    $result = executePlainSQL($query);
+    $result = executePlainSQL("SELECT * FROM DELIVERY WHERE transportStatus = $status");
 
-    if (($row = oci_fetch_row($result)) != false) {
-        echo "<table>";
-        echo "<tr><th>CustomerID</th><th>Total Cost</th><th>Transport Status</th><th>Driver's license number</th><th>Warehouse name</th><th>Destination</th><th>scheduled date</th></tr>";
+    if ((oci_fetch_row($result))) {
+        handleSelectionRequest("Delivery","Delivery.transportStatus = $status");
 
-        while ($row = oci_fetch_array($result, OCI_BOTH)) {
-            echo "<tr><td>" . $row[0] . "</td><td>" . $row[1] . "</td><td>" . $row[2] . "</td><td>" . $row[3] . "</td><td>" . $row[4] . "</td><td>" . $row[5] . "</td><td>" . $row[6]. "</td></tr>";
-
-        }
-        echo "</table>";
-    } else {
+    }
+    else {
         echo "No deliveries found";
     }
 }
