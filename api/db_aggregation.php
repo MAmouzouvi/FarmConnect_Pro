d3b63b17b43bcf4444753d64a5558e65440f8437
UBC Student <nilay17m@students.cs.ubc.ca>
2023-04-05 16:46:59 -0700

    correction

--- api/db_aggregation.php
+++ api/db_aggregation.php
@@ -2,28 +2,28 @@
 function handleAggregationRequest() {
     global $db_conn;
 
-    $date_from = date('Y-m-d');
-    $date_to = date('Y-m-d', strtotime('+2 days'));
+    $date_from = '2023-02-24';
+    $date_to = '2023-02-26'; //date('Y-m-d', strtotime('+2 days'));
 
-    $query = "SELECT deliveryID, COUNT(*)
-              FROM ORA_LPHAM01.Delivery 
+    $query = "SELECT warehouseName, COUNT(*)
+              FROM Delivery 
               WHERE scheduledDate >= TO_DATE('$date_from', 'YYYY-MM-DD') 
               AND scheduledDate <= TO_DATE('$date_to', 'YYYY-MM-DD') 
-              GROUP BY destination 
-              HAVING COUNT(*) > 1";
+              GROUP BY warehouseName 
+              HAVING COUNT(*) > 0";
 
     $result = executePlainSQL($query);
+    outputResultTable($result);
 
-    if (($row = oci_fetch_row($result)) != false) {
-        echo "<table>";
-        echo "<tr><th>Destination Address</th><th>Number of Deliveries</th></tr>";
-        do {
-            echo "<tr><td>" . $row[0] . "</td><td>" . $row[1] . "</td></tr>";
-        } while (($row = oci_fetch_row($result)) != false);
-        echo "</table>";
-    } else {
-        echo "No deliveries found";
-    }
+    // if (($row = oci_fetch_row($result)) != false) {
+    //     echo "<table>";
+    //     echo "<tr><th>Destination Address</th><th>Number of Deliveries</th></tr>";
+    //     do {
+    //         echo "<tr><td>" . $row[0] . "</td><td>" . $row[1] . "</td></tr>";
+    //     } while (($row = oci_fetch_row($result)) != false);
+    //     echo "</table>";
+    // } else {
+    //     echo "No deliveries found";
+    // }
 }
 ?>
-
