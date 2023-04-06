4f6bc8bd06b7cbee89c242fdf1f7987cd90a58aa
UBC Student <nilay17m@students.cs.ubc.ca>
2023-04-05 18:43:20 -0700

    no input taken in function

--- api/db_sorting.php
+++ api/db_sorting.php
@@ -1,30 +1,57 @@
 <?php
-function handleSortingRequest($table, $sortValue, $order)
+require_once('db_connect.php');
+require_once('db_selection.php');
+function handleSortingRequest()
 {
-    if ($sortValue != null) {
-        echo "<h2> Table: delivery (sort value : $sortValue) </h2>";
-        $query = "SELECT * FROM DELIVERY";
-        if ($sortValue == 'customerID' && $order == "asc") {
+    global $db_conn;
+    $error_msg = "<div class=\"error-msg\"> ";
+    if (empty($_GET['sortValue']) || empty($_GET['order'])) {
+        echo $error_msg .= "Please specify a sort value and the order . </div>";
+    } else {
+        $error_msg .= "No deliveries found. </div>";
 
-            $query = "SELECT * FROM DELIVERY ORDER BY customerID ASC";
+        $success = False;
+        $query = <<< QUERY
+        SELECT * FROM DELIVERY ORDER BY :sortValue :order
+        QUERY;
 
-        } else if ($sortValue == 'customerID' && $order == "desc") {
-            $query = "SELECT * FROM DELIVERY ORDER BY customerID DESC";
+        if ($_GET['sortValue'] == 'customerID' && $_GET['order'] == "ASC") {
 
-        } else if ($sortValue == 'totalCost' && $order == "asc") {
-            $query = "SELECT * FROM DELIVERY ORDER BY totalCost ASC";
+            $query = <<< QUERY
+            SELECT * FROM DELIVERY ORDER BY customerID ASC
+            QUERY;
 
-        } else if ($sortValue == 'totalCost' && $order == "desc") {
-            $query = "SELECT * FROM DELIVERY ORDER BY totalCost DESC";
+        } else if ($_GET['sortValue'] == 'customerID' && $_GET['order'] == "DESC") {
+            $query = <<< QUERY
+            SELECT * FROM DELIVERY ORDER BY customerID DESC
+            QUERY;
+
+        } else if ($_GET['sortValue'] == 'totalCost' && $_GET['order'] == "ASC") {
+            $query = <<< QUERY
+            SELECT * FROM DELIVERY ORDER BY totalCost ASC
+            QUERY;
+
+        } else if ($_GET['sortValue'] == 'totalCost' && $_GET['order'] == "DESC") {
+            $query = <<< QUERY
+            SELECT * FROM DELIVERY ORDER BY totalCost DESC
+            QUERY;
 
         }
 
-        $result = executePlainSQL($query);
-        outputResultTable($result);
-    } else {
-        echo "No deliveries found";
-    }
+        $stid = oci_parse($db_conn, $query);
 
+
+        $success = oci_execute($stid);
+        if (!$success) {
+            echo $error_msg;
+//            $e = OCI_Error($statement);
+//            echo htmlentities($e['message']);
+            echo "<br>";
+        } else {
+            outputResultTable($stid);
+        }
+        oci_free_statement($stid);
+    }
 }
 
 ?>
